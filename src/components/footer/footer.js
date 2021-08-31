19495ca058c4431eb70f38d1fc7065f2bad93908
Camilo <camilo3@pdx.edu>
2021-08-30 22:39:03 -0700

    deleted some stuff per a tutorial...

--- src/components/footer/footer.js
+++ src/components/footer/footer.js
@@ -9,15 +9,23 @@ function Footer() {
       </div>
       <div className="right box">
         <SocialIcon
+          style={{ height: 40, width: 40 }}
           className="social"
           url="https://www.linkedin.com/in/camilo-schaser-hughes-706861bb/"
         />
-        <SocialIcon className="social" url="https://github.com/camilionaire" />
         <SocialIcon
+          style={{ height: 40, width: 40 }}
+          className="social"
+          bgColor="#ff5a01"
+          url="https://github.com/camilionaire"
+        />
+        <SocialIcon
+          style={{ height: 40, width: 40 }}
           className="social"
           url="https://www.facebook.com/camilo.schaserhughes"
         />
         <SocialIcon
+          style={{ height: 40, width: 40 }}
           className="social"
           url="https://www.instagram.com/geekcore84/"
         />
